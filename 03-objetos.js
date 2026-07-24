08e70e6f1fc6f9f6b6e689e540248f6b1be3fbd6
GiBischoff <gi_bischoff99@hotmail.com>
2026-07-24 16:59:10 -0300

    Add pessoa and mochila objects with properties

---
+++ 03-objetos.js
@@ -0,0 +1,16 @@
+const pessoa = {
+  nome: "Giovana",
+  idade: 23,
+}
+
+
+const mochila = {
+  // chave : valor
+  garrafa: "Água",
+  caderno: "Português",
+  notebook: "MacBook",
+}
+
+const nome = "Raphael"
+console.log(mochila.caderno)
+console.log(mochila.garrafa)
